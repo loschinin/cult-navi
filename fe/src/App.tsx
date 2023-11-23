839a1ed1368eefa31dc4a5e43ce35db84e8123a4
dmitryloschinin <loschinin@list.ru>
2023-11-23 09:28:31 +0300

    spb museums list

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -3,7 +3,18 @@ import { MainContainer } from "./MainContainer";
 import { Header } from "./components/Header";
 import { MainInput } from "./components/MainInput";
 
-export const MOCK_MUSEUMS = ["Art Museum", "Russian Museum"];
+export const MOCK_MUSEUMS = [
+  "Государственный Эрмитаж",
+  "Музей Фаберже",
+  "Государственный Русский музей",
+  "Летний сад",
+  "Государственный музей городской скульптуры",
+  "Музей-усадьба П. П. Чистякова",
+  "Музей-квартира А. И. Куинджи",
+  "Музей современного искусства Эрарта",
+  "Новый музей",
+  "Музей прикладного искусства СПбГХПА им.А.Л.Штиглица",
+];
 const App = () => {
   const [value, setValue] = useState<string | null>(MOCK_MUSEUMS[0]);
   return (
