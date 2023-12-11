4f3f5fa4f3f9ed7c88a21f923ffa1e6445e6f27f
pavel <pavel_b@inbox.ru>
2023-12-12 00:56:23 +0300

    Парсинг, добавление информации по музеям

--- be/parsing/get_run_schedule.py
+++ be/parsing/get_run_schedule.py
@@ -2,6 +2,10 @@ import time
 from get_rus_museum import get_rus_museum
 from get_hermitage import get_hermitage
 from get_faberzhe import get_faberzhe
+from get_summer_garden import get_summer_garden
+from get_state_museum_of_urban_sculpture import get_state_museum_of_urban_sculpture
+from get_museum_apartment_of_kuindzhi import get_museum_apartment_of_kuindzhi
+from get_erarta import get_erarta
 
 # По расприсанию через определенный интервал времени будет вызываться эта функция
 # для обновления данных с сайта и записи их в каталог parsing/data
@@ -13,12 +17,24 @@ def run_schedule(interval):
             rus_museum_path = get_rus_museum()
             hermitage_path = get_hermitage()
             faberzhe_path = get_faberzhe()
+            summer_garden_path = get_summer_garden()
+            state_museum_of_urban_sculpture_path = get_state_museum_of_urban_sculpture()
+            museum_apartment_of_kuindzhi_path = get_museum_apartment_of_kuindzhi()
+            erarta_path = get_erarta()
             if rus_museum_path:
                 print(f'Data was successfully saved to {rus_museum_path}')
             if hermitage_path:
                 print(f'Data was successfully saved to {hermitage_path}')
             if faberzhe_path:
                 print(f'Data was successfully saved to {faberzhe_path}')
+            if summer_garden_path:
+                print(f'Data was successfully saved to {summer_garden_path}')
+            if state_museum_of_urban_sculpture_path:
+                print(f'Data was successfully saved to {state_museum_of_urban_sculpture_path}')
+            if museum_apartment_of_kuindzhi_path:
+                print(f'Data was successfully saved to {museum_apartment_of_kuindzhi_path}')
+            if erarta_path
+                print(f'Data was successfully saved to {erarta_path}')
             else:
                 print('Failed to save data.')
             print(f"Waiting for {interval} seconds.")
