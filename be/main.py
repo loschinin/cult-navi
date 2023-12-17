8739e1c6efeb555f5914850f77ef93e0ee031308
SanchrTV <92468853+SanchrTV@users.noreply.github.com>
2023-12-17 18:47:52 +0300

    Add llm

--- be/main.py
+++ be/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from langchain.retrievers import WikipediaRetriever
+from langchain.chat_models.gigachat import GigaChat
+from langchain.chains import ConversationalRetrievalChain
 
 app = FastAPI()
 
@@ -13,6 +16,14 @@ app.add_middleware(
     allow_methods=["*"],  # Разрешает все методы
     allow_headers=["*"],  # Разрешает все заголовки
 )
+
+
+def llm_answer(question:str, chat_history:list, qa) -> str:
+    result = qa({"question": question, "chat_history": chat_history})
+    chat_history.append((question, result["answer"])) 
+    return result['answer']
+
+giga = GigaChat(credentials='YWEyOWY4Y2EtYWI0MC00M2RlLWEzZDQtY2VkZTUwYzdhYTFhOjU4ODUwNTkxLTkzMjAtNGNiOC05YWZlLTQ1YjFjMmY3ODdiMw==', verify_ssl_certs=False)
 
 # Музеи, с которыми мы работаем.
 museums_info = [
@@ -26,6 +37,8 @@ museums_info = [
     {"id": 8, 'name': 'Новый музей', "url": "https://ru.wikipedia.org/wiki/Новый_музей_(Санкт-Петербург)"},
     {"id": 9, 'name': 'Музей прикладного искусства СПбГХПА им.А.Л.Штиглица', "url": "https://ru.wikipedia.org/wiki/Музей_А._Л._Штиглица"},
 ]
+retriever = WikipediaRetriever(lang='ru', load_max_docs=2)
+chat_history = []
 
 @app.get("/museums")
 async def get_museums():
@@ -45,6 +58,8 @@ async def query_museum(museum_request: MuseumRequest):
     new_context_df = pd.read_csv(f'./museums_data.csv')
 
     filtered_df = new_context_df[new_context_df['name'] == museum_request.name]
+    docs = retriever.get_relevant_documents(query=museum_request.name, lang="ru")
+    qa = ConversationalRetrievalChain.from_llm(giga, retriever=retriever)
 
     # Check if any data is found
     if not filtered_df.empty:
@@ -64,8 +79,8 @@ async def query_museum(museum_request: MuseumRequest):
        response_from_neural_network = 'Пожалуйста, выберите музей из списка наверху.'
     else:
         # TODO: удалить после того, как нейросеть будет подключена и заменить на нормальный ответ.
-        response_from_neural_network = f'Привет! Это нейросеть. Ты в {museum_request.name} завтра идешь? Билет возьми, я себе уже купила'
-
+        # response_from_neural_network = f'Привет! Это нейросеть. Ты в {museum_request.name} завтра идешь? Билет возьми, я себе уже купила'
+        response_from_neural_network = llm_answer(museum_request.prompt, chat_history, qa)
 
 
     return {"response": response_from_neural_network}
